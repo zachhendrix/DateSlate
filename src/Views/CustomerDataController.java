1b2a99a7ec1364e4ba5c39adb1f19545ae1ca5ba
zach hendrix <51380493+zachhendrix@users.noreply.github.com>
2020-11-19 10:38:26 -0600

    0.8

--- src/Views/CustomerDataController.java
+++ src/Views/CustomerDataController.java
@@ -9,6 +9,7 @@ import Model.Clientele;
 import Model.Customer;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,12 +17,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 /**
@@ -35,21 +39,21 @@ public class CustomerDataController implements Initializable
     Parent scene;
 
     @FXML
-    private TableView<?> customerTableView;
+    private TableView<Customer> customerTableView;
     @FXML
-    private TableColumn<?, ?> customerIDCol;
+    private TableColumn<Customer, Integer> customerIDCol;
     @FXML
-    private TableColumn<?, ?> firstNameCol;
+    private TableColumn<Customer, String> firstNameCol;
     @FXML
-    private TableColumn<?, ?> lastNameCol;
+    private TableColumn<Customer, String> lastNameCol;
     @FXML
-    private TableColumn<?, ?> addressCol;
+    private TableColumn<Customer, String> addressCol;
     @FXML
-    private TableColumn<?, ?> postalCol;
+    private TableColumn<Customer, Integer> postalCol;
     @FXML
-    private TableColumn<?, ?> phoneCol;
+    private TableColumn<Customer, Integer> phoneCol;
     @FXML
-    private TextField customerIDText;
+    private Label customerIDLabel;
     @FXML
     private TextField firstNameText;
     @FXML
@@ -69,6 +73,8 @@ public class CustomerDataController implements Initializable
     @FXML
     private Button addButton;
     @FXML
+    private Button modifyButton;
+    @FXML
     private Button deleteButton;
     @FXML
     private Button cancelButton;
@@ -81,27 +87,63 @@ public class CustomerDataController implements Initializable
     @Override
     public void initialize(URL url, ResourceBundle rb) 
     {
-        // TODO
+        customerIDLabel.setText(String.valueOf(Clientele.getCustomerIDCount() + 1));
+        customerTableView.setItems(Clientele.getAllCustomers());
+        customerIDCol.setCellValueFactory(new PropertyValueFactory<>("customerID"));
+        firstNameCol.setCellValueFactory(new PropertyValueFactory<>("firstName"));  
+        lastNameCol.setCellValueFactory(new PropertyValueFactory<>("lastName")); 
+        addressCol.setCellValueFactory(new PropertyValueFactory<>("address"));   
+        postalCol.setCellValueFactory(new PropertyValueFactory<>("postalCode")); 
+        phoneCol.setCellValueFactory(new PropertyValueFactory<>("phone")); 
     }    
 
     @FXML
     private void addButtonClicked(ActionEvent event) 
     {
-        int customerID = Integer.parseInt(customerIDText.getText());
+        int customerID = Clientele.getCustomerIDCount() + 1 ;
+        customerIDLabel.setText(String.valueOf(customerID + 1));
+        
         String firstName = firstNameText.getText();
         String lastName = lastNameText.getText();
         String address = addressText.getText();
         int postalCode = Integer.parseInt(postalText.getText());
-        String country = countryComboBox.getText();
-        String state = regionComboBox.getText();
+        String country = countryComboBox.getSelectionModel().toString();
+        String state = regionComboBox.getSelectionModel().toString();
         int phone = Integer.parseInt(phoneText.getText());
+        
         Clientele.addCustomer(new Customer(customerID, firstName,lastName,address,postalCode,country, state, phone));
         
     }
+    
+    @FXML
+    void modifyButtonClicked(ActionEvent event) 
+    {
+        Customer customerRef = customerTableView.getSelectionModel().getSelectedItem();
+        customerIDLabel.setText(String.valueOf(customerRef.getCustomerID()));
+        firstNameText.setText(String.valueOf((customerRef).getFirstName()));
+        lastNameText.setText(String.valueOf((customerRef).getLastName()));
+        addressText.setText(String.valueOf((customerRef).getAddress()));
+        postalText.setText(String.valueOf((customerRef).getPostalCode()));
+
+        phoneText.setText(String.valueOf((customerRef).getPhone()));
+    }
+
 
     @FXML
     private void deleteButtonClicked(ActionEvent event) 
     {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Dialog");
+        alert.setHeaderText("Selected Part Will Be Deleted");
+        alert.setContentText("Are you ok with this?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        
+        if (result.get() == ButtonType.OK)
+        {
+            Customer customer = customerTableView.getSelectionModel().getSelectedItem();
+            Clientele.deleteCustomer(customer);
+        } 
         
     }
 
